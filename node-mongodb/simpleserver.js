2df9e1e018e9dfe94ca72e13317a0ed3a5a08883
Viturino da Silva <rosl@gft.com>
2016-11-03 13:26:45 -0200

    schema com mongodb

--- node-mongodb/simpleserver.js
+++ node-mongodb/simpleserver.js
@@ -2,7 +2,7 @@ var MongoClient= require('mongodb').MongoClient,
  assert= require('assert');
 
 //Conection URL
-var url = 'mongodb://localhost:27017/conFusion';
+var url = 'mongodb://localhost:27017/confusion';
 
 // Use connect method to connect to the Server
 MongoClient.connect(url,function (err,db) {
