a22a2ec9e2a54f42069616a5f3c96fc8535750e7
Viturino da Silva <rosl@gft.com>
2016-11-03 10:17:20 -0200

    node-mongodb_conexao e operações

---
+++ node-mongodb/simpleserver.js
@@ -0,0 +1,28 @@
+var MongoClient= require('mongodb').MongoClient,
+ assert= require('assert');
+
+//Conection URL
+var url = 'mongodb://localhost:27017/conFusion';
+
+// Use connect method to connect to the Server
+MongoClient.connect(url,function (err,db) {
+assert.equal(err,null);
+console.log("Conectado");
+
+var collection= db.collection("dishes");
+collection.insertOne({nome:'Renato', description:'Teste'},function(err,result){
+  assert.equal(err,null);
+  console.log("After Insert:");
+  console.log(result.ops);
+
+  collection.find({}).toArray(function(err,docs){
+    assert.equal(err,null);
+    console.log("Encontrado: ");
+    console.log(docs);
+    db.dropCollection("dishes",function(err,result){
+      assert.equal(err,null);
+      db.close();
+    });
+  });
+});
+    });
